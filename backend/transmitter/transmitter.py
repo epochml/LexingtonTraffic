1c5477266069f7058e0694aa484e51169ed50fd4
Cinna Davis <1337isnot1337@users.noreply.github.com>
2025-03-06 21:34:17 -0600

    added python venv. added demonstration transmitter python api with example docs

---
+++ backend/transmitter/transmitter.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+import json
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def welcome():
+    return "<p>Hello, use the api pls at /api or check docs at /docs</p>"
+
+@app.route("/api/lastx")
+def get_last_x():
+    num = int(request.args.get('num'))
+    if num is None:
+        return "ERR: Missing parameter", 400
+
+    with open('../dummydata.json', 'r') as file: # switch to the actual data when we need to
+        data = json.load(file)  
+    
+    data = list(data.items())[-num:]
+    data.reverse()
+    return dict(data)
+@app.route("/api/getrange")
+def get_range():
+    start = int(request.args.get('start'))
+    stop = int(request.args.get('stop'))
+    if start is None or stop is None:
+        return "ERR: Missing parameter", 400
+
+    with open('../dummydata.json', 'r') as file: # switch to the actual data when we need to
+        data = json.load(file)  
+    
+    data = list(data.items())[start:stop + 1]
+    data.reverse()
+    return dict(data)
+
+@app.route("/docs")
+def get_docs():
+    return "<h1>Docs</h1><h2>/api/lastx</h2><p>returns the last x pieces of data. access by using /api/lastx?num=z where z is the last z pieces of data you want.</p><h2>/api/getrange</h2><p>returns the pieces of data in range y z. access by using /api/getrange?start=y&stop=z where y is the first piece of data and z is the last.</p>"
+
+
+if __name__ == '__main__':
+    app.run()
